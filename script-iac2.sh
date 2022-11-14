38e4fae23397f6116828f49520900afebaaf7efc
wesl2x <wesl2x@yahoo.com.br>
2022-11-14 01:59:22 +0000

    arquivos projeto 2

---
+++ script-iac2.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "Atualizando o servidor"
+
+apt-get update
+apt-get upgrade -y
+apt-get install apache2 -y
+apt-get install unzip -y
+
+echo "Baixando a aplicação do endereço"
+
+cd /tmp
+wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
+unzip main.zip
+cd linux-site-dio-main
+cp ./* /var/www/html/ -R
